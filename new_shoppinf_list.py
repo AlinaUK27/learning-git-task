5bcaa873fa6a3397db36cd3f48edf47d8dec5972
Alina Somak <mishchya@gmail.com>
2023-03-01 19:49:26 +0100

    Del second list in shopping_dict

---
+++ new_shoppinf_list.py
@@ -0,0 +1,15 @@
+print("Cписок покупок")
+shopping_dict = {"пекарня":['хліб', 'булочки', 'пончик'],
+"продуктовий": ['морква', 'селера', 'рукола']
+}
+ 
+for keys, values in shopping_dict.items():
+    print(f"Заходжу в {keys.title()}, купую тут такі товари:{values.__str__().title()}")
+value1 = shopping_dict["пекарня"]
+value2 = shopping_dict["продуктовий"]
+print(f"Разом купую {len(value1) + len(value2)} товарів")
+
+del shopping_dict ["продуктовий"]
+
+for keys, values in shopping_dict.items():
+    print(f"Заходжу в {keys.title()}, купую тут такі товари:{values.__str__().title()}")
